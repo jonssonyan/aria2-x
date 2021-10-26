c751968225fb88fbda0227b1e16a5a7ccdcd6d6f
YanZhuangZhuang <yz808@outlook.com>
2021-10-26 15:47:38 +0800

    fix: view

--- renderer.js
+++ renderer.js
@@ -1,11 +1,11 @@
-const { shell } = require('electron');
 window.onload = () => {
-    let aLink = document.getElementById('aLink');
-    aLink.onclick = function (e) {
-        // 阻止默认事件
-        e.preventDefault();
-        let aHref = this.getAttribute('href');
-        // 通过 shell 打开链接
-        shell.openExternal(aHref);
-    }
+    const {shell} = require('electron');
+    const links = document.querySelectorAll('#aLink');
+    links.forEach(link => {
+        link.addEventListener('click', e => {
+            const url = link.getAttribute('href');
+            e.preventDefault();
+            shell.openExternal(url);
+        });
+    });
 }
